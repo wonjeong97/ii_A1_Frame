4462c6256c65604e10c362686116fd81f96b5b1e
WonJeong <wonjeong@huliac.com>
2026-03-02 02:06:16 +0900

    Update StorageCleaner.cs

--- Assets/My/Scripts/Utils/StorageCleaner.cs
+++ Assets/My/Scripts/Utils/StorageCleaner.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.IO;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
@@ -16,35 +17,42 @@ namespace My.Scripts.Utils
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         private static void RunCleanupOnStartup()
         {
-            UniTask.RunOnThreadPool(() =>
+            try
             {
-                Debug.Log($"[StorageCleaner] 백그라운드 자동 정리 시작 (보관 기준: {MaxKeepDays}일)");
-
-                // 루트 경로 가져오기
-                string dataPath = Application.dataPath;
-                DirectoryInfo parentDir = Directory.GetParent(dataPath);
-                string rootPath = parentDir != null ? parentDir.FullName : dataPath;
-
-                // 검사할 타겟 최상위 폴더 경로들
-                string[] targetFolders = {
-                    Path.Combine(rootPath, "Pictures"),
-                    Path.Combine(rootPath, "Timelapse", "Realtime_Video"),
-                    Path.Combine(rootPath, "Timelapse", "Timelapse_Video"),
-                    Path.Combine(rootPath, "Timelapse", "Realtime_Source"),
-                    Path.Combine(rootPath, "Timelapse", "Timelapse_Source")
-                };
-
-                // 기준 날짜 계산
-                DateTime thresholdDate = DateTime.Now.Date.AddDays(-MaxKeepDays);
-
-                foreach (string folderPath in targetFolders)
+                UniTask.RunOnThreadPool(() =>
                 {
-                    CleanOldFolders(folderPath, thresholdDate);
-                }
+                    Debug.Log($"[StorageCleaner] 백그라운드 자동 정리 시작 (보관 기준: {MaxKeepDays}일)");
 
-                Debug.Log("[StorageCleaner] 백그라운드 자동 정리 완료");
+                    // 루트 경로 가져오기
+                    string dataPath = Application.dataPath;
+                    DirectoryInfo parentDir = Directory.GetParent(dataPath);
+                    string rootPath = parentDir != null ? parentDir.FullName : dataPath;
+
+                    // 검사할 타겟 최상위 폴더 경로들
+                    string[] targetFolders = {
+                        Path.Combine(rootPath, "Pictures"),
+                        Path.Combine(rootPath, "Timelapse", "Realtime_Video"),
+                        Path.Combine(rootPath, "Timelapse", "Timelapse_Video"),
+                        Path.Combine(rootPath, "Timelapse", "Realtime_Source"),
+                        Path.Combine(rootPath, "Timelapse", "Timelapse_Source")
+                    };
+
+                    // 기준 날짜 계산
+                    DateTime thresholdDate = DateTime.Now.Date.AddDays(-MaxKeepDays);
+
+                    foreach (string folderPath in targetFolders)
+                    {
+                        CleanOldFolders(folderPath, thresholdDate);
+                    }
+
+                    Debug.Log("[StorageCleaner] 백그라운드 자동 정리 완료");
                 
-            }).Forget(); // .Forget()을 붙여 비동기 작업이 백그라운드에서 안전하게 독립적으로 실행되도록 명시
+                }).Forget(); // .Forget()을 붙여 비동기 작업이 백그라운드에서 안전하게 독립적으로 실행되도록 명시
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"[StorageCleaner] 정리 작업 중 예외 발생: {e.Message}");
+            }
         }
 
         private static void CleanOldFolders(string targetPath, DateTime thresholdDate)
@@ -57,7 +65,7 @@ namespace My.Scripts.Utils
             foreach (DirectoryInfo subDir in subDirs)
             {
                 // 폴더 이름이 "yyyy-MM-dd" 형식의 날짜인지 파싱 시도
-                if (DateTime.TryParseExact(subDir.Name, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime folderDate))
+                if (DateTime.TryParseExact(subDir.Name, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime folderDate))
                 {
                     // 폴더의 날짜가 기준 날짜보다 이전(과거)이라면 삭제
                     if (folderDate.Date < thresholdDate)
